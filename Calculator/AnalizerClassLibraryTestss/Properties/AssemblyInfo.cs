b470e9d9cab660fa94e6f8fb7d6cb366b00c0336
Daryna Riabokin <darynariabokin@gmail.com>
2023-12-15 21:22:02 +0200

    init

---
+++ Calculator/AnalizerClassLibraryTestss/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalizerClassLibraryTestss")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalizerClassLibraryTestss")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0839d4d7-590c-41ac-b4af-7443b529ff1d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calculator/AnalizerClassLibraryTestss/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using AnalaizerClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+
+namespace AnalaizerClassLibrary.Tests
+{
+    [TestClass]
+    public class AnalaizerClassTests
+    {
+        [TestMethod]
+        [DataSource("System.Data.SqlClient", "Data Source=DARYNA-PC\\SQLEXPRESS01;Initial Catalog=Lab_test;Integrated Security=True", "CheckCurrencyTable", DataAccessMethod.Sequential)]
+        public void CheckCurrency_ExpressionValidity()
+        {
+            // Arrange
+            string expression = TestContext.DataRow["expression"].ToString();
+            bool expectedResult = bool.Parse(TestContext.DataRow["result"].ToString());
+
+            AnalaizerClass.expression = expression;
+
+            // Act
+            bool result = AnalaizerClass.CheckCurrency();
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        public TestContext TestContext { get; set; }
+    }
+}
